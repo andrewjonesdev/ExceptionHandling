cceaa9d9911d303ecf2a19668eca5da70799c425
Andrew Jones <jones.gaffney1920@gmail.com>
2017-10-24 01:02:11 -0500

    Add project files.

---
+++ ExceptionHandling/Program.cs
@@ -0,0 +1,83 @@
+﻿
+using System;
+using System.IO;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                var calculator = new Calculator();
+                var result = calculator.Divide(5, 0);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Sorry, an unexpected divide by zero error occurred.");
+            }
+            catch (ArithmeticException ex)
+            {
+                Console.WriteLine("Sorry, an unexpected arithmetic error occurred.");
+            }
+            catch (SystemException ex)
+            {
+                Console.WriteLine("Sorry, an unexpected system error occurred.");
+            }
+            catch (Exception ex) //variable for exception not required just the Exception class but by having the variable you can call it later
+            {
+                Console.WriteLine("Sorry, an unexpected error occurred.");
+            }
+            finally //great for cleaning up unmanaged resources
+            {
+                Console.WriteLine("Finally.");
+            }
+
+            //dynamic streamReader = 0;
+            StreamReader streamReader = null;
+            Console.WriteLine("Test");
+            try
+            {
+                streamReader = new StreamReader(@"c:\file.zip");
+                var content = streamReader.ReadToEnd();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, an unexpected error occurred.");
+            }
+            finally //great for cleaning up unmanaged resources
+            {
+                if (streamReader != null)
+                {
+                    streamReader.Dispose();
+                    Console.WriteLine("Successfully Disposed!");
+                }
+            }
+
+            try
+            {
+                using (var streamReader1 = new StreamReader(@"c:\file.zip")) //automatically sets up hidden finally and dispose this is the prefferable method
+                {
+                    var content = streamReader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Sorry, an unexpected error occurred.");
+            }
+            
+
+            try
+            {
+                var api = new YouTubeApi();
+                var videos = api.GetVideos("mosh");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ ExceptionHandling/YouTubeApi.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionHandling
+{
+    public class YouTubeApi
+    {
+        public List<Video> GetVideos(string user)
+        {
+            try
+            {
+                // Access YouTube web service 
+                // Read the data 
+                // Create a list of Video objects
+                throw new Exception("Oops some low level YouTube error.");//remove this so return statement is reachable just a testing example
+            }
+            catch (Exception ex)
+            {
+                // Log 
+
+                throw new YouTubeException("Could not fetch the videos from YouTube.", ex); //wraps the low level exceptions into this
+            }
+
+            return new List<Video>();
+        }
+    }
+}
